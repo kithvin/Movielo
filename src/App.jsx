f276ea27f67b6934b851137d6c240e9d47964307
kithvin <kithvinbandara1994@gmail.com>
2025-03-23 23:10:27 +0530

    Create header section with logo and navigation menu for Movielo

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,10 @@
 import React from "react";
+import Home from './Screen/Home'
 
 const App = () => {
-  return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <div className="bg-white p-6 rounded-2xl shadow-lg text-center">
-        <h1 className="text-3xl font-bold text-blue-600">Hello, Tailwind CSS!</h1>
-        <p className="text-gray-700 mt-2">This is a simple React component styled with Tailwind.</p>
-        <button className="mt-4 px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition duration-300">
-          Click Me
-        </button>
-      </div>
-    </div>
-  );
+  return <>
+  <Home/>
+  </>;
 };
 
 export default App;
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,9 +1,57 @@
-import React from 'react'
+import React, { useState } from "react";
+import logo from "../IMG/logo.png";
+import { IoSearch } from "react-icons/io5";
 
 function Header() {
+  const menu = [
+    {
+      id: 1,
+      name: "Home",
+    },
+    {
+      id: 2,
+      name: "Movies",
+    },
+    {
+      id: 3,
+      name: "TV",
+    },
+    {
+      id: 4,
+      name: "Sports",
+    },
+    {
+      id: 5,
+      name: "News",
+    },
+    {
+      id: 6,
+      name: "Hubs",
+    },
+  ];
+
   return (
-    <div>Header</div>
-  )
+    <div className="flex">
+      <img src={logo} className="w-[120px]" />
+      <ul className="flex gap-8">
+        {menu.map((item) => (
+          <li
+            className="text-gray-400 text-[18px] 
+                font-medium cursor-pointer mb-3 mt-2 
+                 hover:bg-gray-600 hover:text-white
+                px-3 pb-2 py-1 rounded-md transition-all duration-500"
+          >
+            {item.name}
+          </li>
+        ))}
+      </ul>
+      <div>
+      <IoSearch className='text-[35px] text-gray-300 
+            hover:bg-gray-700 px-[3px] pb-[2px] py-[2px] cursor-pointer
+            rounded-md hover:text-white transition-all duration-500 ease-in-out' />
+      </div>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
